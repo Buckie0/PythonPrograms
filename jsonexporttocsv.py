1d07afb81c00721ea4b3620a9e1940ead7dccd2e
Matt Buck <MacBuck@MacBuck.cable.virginmedia.net>
2019-02-04 19:34:03 +0000

    Buckie0 Commit

--- jsonexporttocsv.py
+++ jsonexporttocsv.py
@@ -1,46 +1,48 @@
 import requests
 import csv
 import json
+from nested_lookup import nested_lookup
 
 # job_event data
 url = "http://m_buck:b7SqUA4yB7qf@ansible.localnet/api/v2/jobs/"
 job = input('Enter Job ID: ')
-end = "/job_events/"
-stdout = "/stdout/"
+end = "/job_events/?page="
+page = 0
 
+print("Creating .txt file for job " + job)
 
-def result(var):
-    r = requests.get(url + job + end)
-    data = r.json()
-    res = data['results'][(var)]['event_data']['res']['stdout_lines']
-    return res
-
-
-def stdoutres(var1):
-    r = requests.get(url + job + stdout)
-    data1 = r.json()
-    return data1
-
-# print(result(3))
-# print(result(5))
-# print(result(7))
-# print(result(9))
-
-print("Creating .csv file for job " + job)
-
-# csvjob = 'Job' + job + '.csv'
+csvjob = 'Job' + job + '.csv'
 textjob = 'Job' + job + '.txt'
 
-# with open(csvjob, 'w', newline='') as f:
-#     writer = csv.writer(f, delimiter=',')
-#     writer.writeheader()
-#     writer.writerow(result(3))
-#     writer.writerow(result(5))
-#     writer.writerow(result(7))
-#     writer.writerow(result(9))
+while True:
+    page += 1
+    r = requests.get(url + job + end + str(page))
+    data = r.json()
+    result1 = nested_lookup("stdout_lines", data)
+    response = r.status_code
+    if response == "200":
+        page += 1
+        print("Page " + str(page))
+        with open(textjob, 'a') as f:
+            out = json.dump(result1, f, indent=1)
+        continue
+    elif response != "200":
+        break
 
-with open(textjob, 'w', newline='') as f:
-    json.dump(result(3), f)
-    json.dump(result(5), f)
-    json.dump(result(7), f)
-    json.dump(result(9), f)
+
+# Testing
+
+r = requests.get(url + job + end + str(page))
+data = r.json()
+result1 = nested_lookup("stdout_lines", data)
+response = r.status_code
+
+for x in result1:
+    if response == "200":
+        page += 1
+        print("Page " + str(page))
+        with open(csvjob, 'w') as f:
+            fields = ["Device Name", "Command"]
+            writer = csv.DictWriter(f, fieldnames=fields)
+            writer.writeheader()
+            writer.writerows(result1)
