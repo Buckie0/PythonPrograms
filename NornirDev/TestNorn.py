c8292f59e8b74d388e039faa8566c7a69383e4ee
m_buck <matt.buck@redcentricplc.com>
2019-03-04 17:10:22 +0000

    Buckie0 commit

--- NornirDev/TestNorn.py
+++ NornirDev/TestNorn.py
@@ -1,21 +1,47 @@
-from nornir.core import InitNornir
+from colored import attr, fg
+
+from nornir import InitNornir
+# from nornir.plugins.functions.text import print_result
 from nornir.plugins.tasks.networking import netmiko_send_command
-from nornir.plugins.functions.text import print_result
-from colored import fg, attr
 
 nornir_host = InitNornir(config_file='config.yaml')
 
 print(nornir_host.inventory.hosts)
-# print('\n Checking Duplex, Please Wait...')
-# duplex = nornir_host.run(
-#     task=netmiko_send_command,
-#     command_string='sh int status'
-# )
-# if "half" in duplex:
-#     print('''%s\n  HALF DUPLEX DISCOVERED.
-#   PLEASE CONNECT TO DEVICE AND INVESTIGATE (sh int status)%s
-#   ''' % (fg(1), attr(0)))
-# elif duplex:
-#     print('%s\n  No issues found%s' % (fg(2), attr(0)))
 
+print('\n Checking Duplex, Please Wait...')
+duplex = nornir_host.run(
+    task=netmiko_send_command,
+    command_string='sh int status'
+)
 # print_result(duplex)
+if "half" in duplex:
+    print('''%s\n  HALF DUPLEX DISCOVERED.
+  PLEASE CONNECT TO DEVICE AND INVESTIGATE (sh int status)%s
+  ''' % (fg(1), attr(0)))
+elif duplex:
+    print('%s\n  No issues found%s' % (fg(2), attr(0)))
+
+
+print('\n Reading Logs, Please Wait...')
+logcheck = nornir_host.run(
+    task=netmiko_send_command,
+    command_string='sh log'
+)
+if "down" in logcheck:
+    print('''%s\n  DROPS SEEN.
+  PLEASE CONNECT TO DEVICE AND INVESTIGATE (sh log)%s''' % (fg(1), attr(0)))
+elif logcheck:
+    print('%s\n  No drops seen%s' % (fg(2), attr(0)))
+
+
+print('\n Checking interfaces for errors, Please Wait...')
+crc = nornir_host.run(
+    task=netmiko_send_command,
+    command_string='sh run | i CRC'
+)
+if not ("0") in crc:
+    print('''%s\n  INTERFACE ERRORS SEEN.
+  PLEASE CONNECT TO DEVICE AND INVESTIGATE (sh int | i CRC)%s
+  ''' % (fg(1), attr(0)))
+elif duplex:
+    print('%s\n  No issues found%s' % (fg(2), attr(0)))
